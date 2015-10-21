550d9e780b96c0e2c41fd662dec2d14ea47f6605
Katrina Guyduy <katrinaguyduy@gmail.com>
2015-10-20 23:44:49 -0700

    added angular form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
+// pulse
+
 $(document).ready(function(){
   $('a').click(function(){
     $('a').toggleClass('red');
   })                  
 })
+
+// form
+
+var formApp = angular.module('formApp', [])
+
+    .controller('formController', function($scope) {
+  
+        // // we will store our form data in this object
+        // $scope.formData = {};
+        $scope.showerrors=false;
+$scope.submitForm= function(){
+
+if ($scope.form.$invalid){
+	$scope.showErrors=true;
+}
+};
+       
+    });
